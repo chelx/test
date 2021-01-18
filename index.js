14c6f0993a6be0121394c1e45f94e179341e06b8
Vũ Hiền <huyhienvu@gmail.com>
2021-01-18 16:14:19 +0700

    i

--- index.js
+++ index.js
@@ -4,27 +4,32 @@ const fs = require('fs');
 screenshot('https://example.com').then(() => console.log('screenshot saved'));
 
 async function screenshot(url) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-gpu",
-    ]
-  });
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-gpu",
+      ]
+    });
 
-  const page = await browser.newPage();
-  await page.setViewport({width: 1920, height: 1080});
-  await page.goto(url, {
-    timeout: 0,
-    waitUntil: 'networkidle0',
-  });
-  const screenData = await page.screenshot({encoding: 'binary', type: 'jpeg', quality: 100});
-  if (!!screenData) {
-    fs.writeFileSync('screenshots/screenshot.jpg', screenData);
-  } else {
-    throw Error('Unable to take screenshot');
+    const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080});
+    await page.goto(url, {
+      timeout: 0,
+      waitUntil: 'networkidle0',
+    });
+    const screenData = await page.screenshot({encoding: 'binary', type: 'jpeg', quality: 100});
+    if (!!screenData) {
+      fs.writeFileSync('screenshots/screenshot.jpg', screenData);
+    } else {
+      throw Error('Unable to take screenshot');
+    }
+
+    await page.close();
+    await browser.close();
+  } catch (e) {
+    console.log(e)
   }
 
-  await page.close();
-  await browser.close();
 }
